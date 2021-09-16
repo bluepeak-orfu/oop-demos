cbea770286b43ab8132d6b0eedb762696e581f56
Erik Stenbäcka <erik.stenbacka@sigma.se>
2021-09-16 11:04:44 +0200

    Move decision making to its own solution

---
+++ DecisionMakingDemo/DecisionMakingDemo/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+//// Ett "block" skapas med måsvingar
+//// Variabler i ett block går inte att komma åt utanför blocket
+//{
+//    int x = 5;
+//    Console.WriteLine(x);
+//}
+////Console.WriteLine(x); // <-- här kommer vi inte åt x.
+
+//int age = 18;
+//if (age < 18)
+//    Console.WriteLine("Yngre än 18");
+//    Console.WriteLine("Yngre än 18"); // <-- inte del av if:en
+
+//if (age < 18)
+//{
+//    Console.WriteLine("Yngre än 18");
+//    Console.WriteLine("Yngre än 18");
+//    Console.WriteLine("Yngre än 18");
+//}
+
+//// Olika operationer vi kan göra för att jämföra värden
+//bool x1 = age == 18;
+//bool x2 = age != 18;
+//bool x3 = age < 18;
+//bool x4 = age <= 18;
+//bool x5 = age >= 18;
+
+//bool x6 = x1 && x2; // <-- OCH
+//bool x7 = x1 || x2; // <-- ELLER
+//bool x8 = x1 && x2 || x3;
+//bool x9 = x1 && (x2 || x3);
+
+//bool x10 = !x1; // true blir false, false blir true
+
+//// Modulus 2 kan användas för att jämföra jämna och udda siffror
+//if (2 % 2 == 0)
+//{
+//    Console.WriteLine("Det är sant!");
+//}
+
+//Console.ReadLine();
+
+
+
+//// Switch är en annan typ av beslutsfattning
+//var myVariable = 11;
+
+//switch (myVariable)
+//{
+//    case 1:
+//        Console.WriteLine("Variablen är 1");
+//        break;
+//    case 2:
+//        Console.WriteLine("Variablen är 2");
+//        break;
+//    case 3:
+//        Console.WriteLine("Variablen är 3");
+//        break;
+//    case 10:
+//        Console.WriteLine("Variablen är 10");
+//        break;
+//    default:
+//        Console.WriteLine("Inget matchade");
+//        break;
+//}
