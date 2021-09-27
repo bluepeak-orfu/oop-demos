4904dcf6d7168cb9c8f0276c61ec09238fffb3b4
Erik Stenbäcka <erik.stenbacka@sigma.se>
2021-09-27 13:25:13 +0200

    Add demos from week 3 day 1

---
+++ OOGrunderDemo/OOGrunderDemo/Car.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOGrunderDemo
+{
+    class Car
+    {
+        private string _brand;
+        public string _regNumber;
+        private string _color;
+        private int _age;
+
+        public Car() : this("Volvo", "AAA111")
+        {
+            Console.WriteLine("No parameters");
+        }
+
+        public Car(string brand, string regNumber)
+        {
+            Console.WriteLine("Two parameters");
+            _brand = brand;
+            _regNumber = regNumber;
+            _color = "White";
+            _age = 0;
+        }
+
+        public void Repaint(string newColor)
+        {
+            _color = newColor;
+        }
+
+        public bool NeedService()
+        {
+            return _age > 5;
+        }
+
+        public string DisplayName(string prefix, string suffix)
+        {
+            if (prefix == "")
+            {
+                prefix = "$$$";
+            }
+            return $"{prefix} {_brand} {suffix}";
+        }
+
+        public void IncreaseAge()
+        {
+            _age++;
+        }
+    }
+}
---
+++ OOGrunderDemo/OOGrunderDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using OOGrunderDemo;
+using System;
+
+Car car1 = new Car("Volvo", "ABC123");
+//Console.ReadLine();
+//Car car2 = new Car();
+//Console.ReadLine();
+
+var carX = new Car();
+Car carY = new();
+Car carY2 = new("Saab", "BBB222");
+
+car1.Repaint("Yellow");
+bool x = car1.NeedService();
+Console.WriteLine($"Need service 1: {x}");
+car1.IncreaseAge();
+car1.IncreaseAge();
+car1.IncreaseAge();
+car1.IncreaseAge();
+car1.IncreaseAge();
+car1.IncreaseAge();
+bool x2 = car1.NeedService();
+Console.WriteLine($"Need service 2: {x2}");
+
+car1.DisplayName("PRE", "POST");
+car1.DisplayName("", "POST");
+
+//Car car3 = new Car();
+//string brand3 = Console.ReadLine();
+//string regNumber3 = Console.ReadLine();
