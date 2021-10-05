5ef6c6e60051b4d81e46674de8f2ec6330f5155b
Erik Stenbäcka <erik.stenbacka@sigma.se>
2021-10-05 12:51:29 +0200

    Add demo for enums

---
+++ EnumDemo/EnumDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using EnumDemo;
+
+Direction myDirection = Direction.North;
+
+if (myDirection == Direction.South)
+{
+    Console.WriteLine("Going south");
+}
+
+switch (myDirection)
+{
+    case Direction.North:
+        break;
+    case Direction.East:
+        break;
+    case Direction.South:
+        Console.WriteLine("Going south");
+        break;
+    case Direction.West:
+        break;
+}
