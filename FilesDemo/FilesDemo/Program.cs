a596b3d3d565e9f8a7198742b472b0b9cb5ed024
Erik Stenbäcka <erik.stenbacka@sigma.se>
2021-11-01 10:32:37 +0100

    Add project for files demo

---
+++ FilesDemo/FilesDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FilesDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
