b6b1bb43c49bd5b811958389413f731e53810f06
Erik Stenbäcka <erik.stenbacka@sigma.se>
2021-11-01 10:31:36 +0100

    Add more files for web api demo

---
+++ WebApiDemo/WebApiDemo/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiDemo
+{
+    public class Car
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Home { get; set; }
+
+        public Car()
+        {
+            string x = "1";
+        }
+    }
+}
--- WebApiDemo/WebApiDemo/Controllers/CarController.cs
+++ WebApiDemo/WebApiDemo/Controllers/CarController.cs
@@ -8,7 +8,7 @@ namespace WebApiDemo.Controllers
 {
     [ApiController]
     [Route("car")]
-    public class CarController
+    public class CarController : ControllerBase
     {
         [HttpGet("get")] // Blir /car/get
         public string GetCar(int age, string model)
@@ -26,6 +26,47 @@ namespace WebApiDemo.Controllers
         public string ListCar(string country)
         {
             return $"car1 is from {country}";
+        }
+
+        [HttpPost("getclass")]
+        public string GetCarClass(Car car)
+        {
+            return car.Name;
+        }
+
+        [HttpPost("string-demo")]
+        public string StringDemo([FromBody] string value, [FromQuery] int count)
+        {
+            return $"The value is {value} with count {count}";
+        }
+
+        [HttpGet("mycar")]
+        public Car MyCar()
+        {
+            return new Car() {
+                Name = "car1",
+                Age = 17,
+                Home = "qwerty"
+            };
+        }
+
+        [HttpGet("mycar2")]
+        public IActionResult MyCar2(int count)
+        {
+            if (count < 0)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Car car = new Car()
+                {
+                    Name = "car1",
+                    Age = 17,
+                    Home = "qwerty"
+                };
+                return Ok(car);
+            }
         }
     }
 }
