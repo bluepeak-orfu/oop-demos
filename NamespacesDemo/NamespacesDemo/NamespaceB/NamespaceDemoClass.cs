011ec566fc80dfd780d556f46d5738926ecce4f3
Erik Stenbäcka <erik.stenbacka@sigma.se>
2021-11-01 10:30:20 +0100

    Add demo for namespaces

---
+++ NamespacesDemo/NamespacesDemo/NamespaceB/NamespaceDemoClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamespacesDemo.NamespaceB
+{
+    class NamespaceDemoClass
+    {
+        public NamespaceDemoClass()
+        {
+            Console.WriteLine("Namespace: NamespacesDemo.NamespaceB");
+        }
+    }
+}
---
+++ NamespacesDemo/NamespacesDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+//using NamespacesDemo.NamespaceB;
+
+//new NamespaceA.Account();
+//new NamespaceA.SubnamespaceA.Account();
+//new Account();
+//NamespaceDemoClass x = new NamespaceDemoClass();
+
+//namespace NamespaceA
+//{
+//    class Account { }
+
+//    //namespace SubnamespaceA
+//    //{
+//    //    class Account { }
+//    //}
+//}
+//namespace NamespaceA.SubnamespaceA
+//{
+//    class Account { }
+//}
+//class Account { }
+
+
+
+namespace NamespacesDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
