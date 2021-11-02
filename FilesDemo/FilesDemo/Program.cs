87c8e70b7580ebe455ed3dbb96d492bc9cfa1078
Erik Stenbäcka <erik.stenbacka@sigma.se>
2021-11-02 10:11:23 +0100

    Add demo for file streams

--- FilesDemo/FilesDemo/Program.cs
+++ FilesDemo/FilesDemo/Program.cs
@@ -9,48 +9,54 @@ namespace FilesDemo
     {
         static void Main(string[] args)
         {
-            string fileContent = File.ReadAllText("demo1.txt");
-            Console.WriteLine($"Content is: {fileContent}");
-
-            //// Ingen bra idé att skriva ut icke textfiler
-            //string fileContent = File.ReadAllText("FilesDemo.exe");
+            //string fileContent = File.ReadAllText("demo1.txt");
             //Console.WriteLine($"Content is: {fileContent}");
 
-            // Skriver över filen om den finns
-            File.WriteAllText("demo2.txt", "Text from C#");
+            ////// Ingen bra idé att skriva ut icke textfiler
+            ////string fileContent = File.ReadAllText("FilesDemo.exe");
+            ////Console.WriteLine($"Content is: {fileContent}");
 
-            // Lägger till i slutet på filen. Skapar om den inte finns
-            File.AppendAllText("demo2.txt", "More text in the file");
+            //// Skriver över filen om den finns
+            //File.WriteAllText("demo2.txt", "Text from C#");
 
-            string fileName = "demo3.txt";
-            if (!File.Exists(fileName))
-            {
-                File.WriteAllText(fileName, "Header1\tHeader2\n");
-            }
-            else
-            {
-                File.AppendAllText(fileName, "Data1\tData2\n");
-            }
+            //// Lägger till i slutet på filen. Skapar om den inte finns
+            //File.AppendAllText("demo2.txt", "More text in the file");
 
-            string[] lines = File.ReadAllLines("demo4.txt");
-            foreach (string line in lines)
-            {
-                Console.WriteLine($"Line content: {line}");
-            }
-            //string[] lines = File.ReadAllText("demo4.txt").Split("\n");
+            //string fileName = "demo3.txt";
+            //if (!File.Exists(fileName))
+            //{
+            //    File.WriteAllText(fileName, "Header1\tHeader2\n");
+            //}
+            //else
+            //{
+            //    File.AppendAllText(fileName, "Data1\tData2\n");
+            //}
 
-            List<string> inputs = new List<string>();
-            while (true)
-            {
-                Console.WriteLine("Write some input");
-                string input = Console.ReadLine();
-                if (input == "")
-                {
-                    break;
-                }
-                inputs.Add(input);
-            }
-            File.WriteAllLines("demo5.txt", inputs);
+            //string[] lines = File.ReadAllLines("demo4.txt");
+            //foreach (string line in lines)
+            //{
+            //    Console.WriteLine($"Line content: {line}");
+            //}
+            ////string[] lines = File.ReadAllText("demo4.txt").Split("\n");
+
+            //List<string> inputs = new List<string>();
+            //while (true)
+            //{
+            //    Console.WriteLine("Write some input");
+            //    string input = Console.ReadLine();
+            //    if (input == "")
+            //    {
+            //        break;
+            //    }
+            //    inputs.Add(input);
+            //}
+            //File.WriteAllLines("demo5.txt", inputs);
+
+            StreamDemo demo = new StreamDemo();
+            //demo.ReadFromStream();
+            //demo.ReadFromStreamWithUsing();
+            //demo.PrintAllContent();
+            demo.WriteContent(new string[] { "QQQQ", "WWWW", "EEEE" });
         }
     }
 }
---
+++ FilesDemo/FilesDemo/StreamDemo.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilesDemo
+{
+    class StreamDemo
+    {
+        public void ReadFromStream()
+        {
+            FileStream fs = File.OpenRead("demo4.txt");
+            StreamReader sr = new StreamReader(fs);
+            try
+            {
+                Console.WriteLine($"Line: {sr.ReadLine()}");
+                Console.WriteLine($"Line: {sr.ReadLine()}");
+            }
+            finally
+            {
+                sr.Close();
+            }
+            Console.ReadKey();
+        }
+
+        public void ReadFromStreamWithUsing()
+        {
+            FileStream fs = File.OpenRead("demo4.txt");
+            using(StreamReader sr = new StreamReader(fs))
+            {
+                Console.WriteLine($"Line: {sr.ReadLine()}");
+                Console.WriteLine($"Line: {sr.ReadLine()}");
+            }
+            Console.ReadKey();
+        }
+
+        public void PrintAllContent()
+        {
+            FileStream fs = File.OpenRead("demo4.txt");
+            using (StreamReader sr = new StreamReader(fs))
+            {
+                while (true)
+                {
+                    string line = sr.ReadLine();
+                    if (line == null)
+                    {
+                        break;
+                    }
+                    Console.WriteLine($"Line 1: {line}");
+                }
+            }
+
+            // Exempel där vi använder kompakt syntax för att läsa lines
+            FileStream fs2 = File.OpenRead("demo4.txt");
+            using (StreamReader sr = new StreamReader(fs2))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine($"Line 2: {line}");
+                }
+            }
+
+            FileStream fs3 = File.OpenRead("demo4.txt");
+            using (StreamReader sr = new StreamReader(fs3))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    Console.WriteLine($"Line 3: {line}");
+                }
+            }
+        }
+
+        public void WriteContent(string[] contents)
+        {
+            FileStream fs = File.Create("demo6.txt");
+            using (StreamWriter sw = new StreamWriter(fs))
+            {
+                foreach (string content in contents)
+                {
+                    sw.WriteLine(content);
+                }
+            }
+        }
+    }
+}
